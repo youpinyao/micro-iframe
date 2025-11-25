68ff62e15604e8a1e00ba2de05e40485495b5b71
youpinyao <youpinyao@anta.com>
2025-11-25 09:44:11 +0800

    feat: init

--- packages/sdk/src/react.ts
+++ packages/sdk/src/react.ts
@@ -1,4 +1,4 @@
-import type { createBrowserRouter } from 'react-router-dom'
+import { replace, type createBrowserRouter } from 'react-router-dom'
 
 type Router = ReturnType<typeof createBrowserRouter>
 
@@ -10,34 +10,50 @@ export const createSubReactApp = ({ name, router }: { name: string; router: Rout
   const originalNavigate = router.navigate
 
   // 重写 navigate 方法，使用 replace 而不是 push
-  router.navigate = ((to: Parameters<typeof router.navigate>[0], options?: Parameters<typeof router.navigate>[1]) => {
-    return originalNavigate(to, { ...options, replace: true } as Parameters<typeof router.navigate>[1])
+  router.navigate = ((
+    to: Parameters<typeof router.navigate>[0],
+    options?: Parameters<typeof router.navigate>[1]
+  ) => {
+    return originalNavigate(to, {
+      ...options,
+      replace: true,
+      state: {
+        replace: options?.replace,
+      },
+    } as Parameters<typeof router.navigate>[1])
   }) as typeof router.navigate
 
   // 订阅路由状态变化
-  const unsubscribe = router.subscribe((state: { location: { pathname: string; search: string; hash: string }; historyAction: string }) => {
-    const currentPath = state.location.pathname + state.location.search + state.location.hash
+  const unsubscribe = router.subscribe(
+    (state: {
+      location: { pathname: string; search: string; hash: string; state: { replace: boolean } }
+      historyAction: string
+    }) => {
+      const currentPath = state.location.pathname + state.location.search + state.location.hash
 
-    console.log('to', currentPath, state)
-    console.log('--------------')
+      console.log('to', currentPath, state)
+      console.log('--------------')
 
-    window.top!.postMessage(
-      {
-        type: 'route-change',
-        payload: {
-          name,
-          path: currentPath,
+      window.top!.postMessage(
+        {
+          type: 'route-change',
+          payload: {
+            name,
+            path: currentPath,
+            replace: !!state.location.state.replace
+          },
         },
-      },
-      '*'
-    )
-  })
+        '*'
+      )
+    }
+  )
 
   // 监听来自主应用的路由变化消息
   const handleMessage = (event: MessageEvent) => {
     if (event.data.type === 'route-change') {
       const { path } = event.data.payload
-      const currentPath = router.state.location.pathname + router.state.location.search + router.state.location.hash
+      const currentPath =
+        router.state.location.pathname + router.state.location.search + router.state.location.hash
 
       if (path === currentPath) {
         return
@@ -55,4 +71,3 @@ export const createSubReactApp = ({ name, router }: { name: string; router: Rout
     window.removeEventListener('message', handleMessage)
   }
 }
-
